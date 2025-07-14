436b977774012729f60d58e6af8b7774fe3f8b4d
joshkung25 <joshuahkung@gmail.com>
2025-07-13 22:04:44 -0700

    updated refactored code

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from agent.chat import start_chat
-from parser.pdf_parser import parse_pdf_token_chunks
+
+# from parser.pdf_parser import parse_pdf_token_chunks
+from parser.pdf_parser import parse_pdf
 from agent.retriever import add_doc_to_collection
 
 if __name__ == "__main__":
-    resume_chunks = parse_pdf_token_chunks("public/resume.pdf", doc_type="resume")
-    transcript_chunks = parse_pdf_token_chunks("public/transcript.pdf", doc_type="transcript")
+    # resume_chunks = parse_pdf_token_chunks("public/resume.pdf", doc_type="resume")
+    # transcript_chunks = parse_pdf_token_chunks(
+    #     "public/transcript.pdf", doc_type="transcript"
+    # )
 
+    # add_doc_to_collection(resume_chunks, "resume")
+    # add_doc_to_collection(transcript_chunks, "transcript")
+
+    resume_chunks = parse_pdf("public/Josh_Kung_Resume_2025_v4.pdf")
     add_doc_to_collection(resume_chunks, "resume")
+
+    transcript_chunks = parse_pdf(
+        "public/JoshuaKung_AcademicTranscript_Northeastern_2026.pdf"
+    )
     add_doc_to_collection(transcript_chunks, "transcript")
 
     start_chat()
